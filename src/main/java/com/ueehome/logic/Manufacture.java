1d82a1213b86cef26d3c4edf90fe34ef545db647
tangwei <tangwei@ueehome.com>
2017-03-28 14:32:08 +0800

    fix

--- src/main/java/com/ueehome/logic/Manufacture.java
+++ src/main/java/com/ueehome/logic/Manufacture.java
@@ -4,7 +4,7 @@ import com.ueehome.logic.bean.RegisterQQIOTLicenseData;
 
 /**
  * Created by TangWei on 2017/3/27.
- * QQ物联授权管理
+ * 生产
  */
 public class Manufacture {
     public RegisterQQIOTLicenseData registerQQIOTLicense() {
